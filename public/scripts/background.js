94ac0edb042c344c6a5f2a6dcb4174ba784bcc6d
UzumakiKurama <bbgiabhijeet12@gmail.com>
2021-12-26 22:39:54 +0530

    Fixed background slider

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,18 +1,25 @@
 var counter = 0;
-function changeBG(){
-    var imgs = [
-        "url(./images/949067.jpg)",
-        "url(./images/1035095.png)",
-        "url(./images/1061011.jpg)",
-        "url(./images/859804.png)",
-        "url(./images/39041.jpg)",
-        "url(./images/31513.jpg)",
-      ]
-    
-    if(counter === imgs.length) counter = 0;
+const img1 = document.getElementById("img-1");
+const img2 = document.getElementById("img-2");
+const img3 = document.getElementById("img-3");
+const img4 = document.getElementById("img-4");
+const img5 = document.getElementById("img-5");
+
+let imgs = [
+  `url(${img1.src})`,
+  `url(${img2.src})`,
+  `url(${img3.src})`,
+  `url(${img4.src})`,
+  `url(${img5.src})`,
+];
+console.log(imgs);
+function changeBG() {
+  if (imgs.length === 5) {
+    if (counter === imgs.length) counter = 0;
     $("body").css("background-image", imgs[counter]);
 
     counter++;
+  }
 }
-  
-  setInterval(changeBG, 4000);
+
+setInterval(changeBG, 4000);
